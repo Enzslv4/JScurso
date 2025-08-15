c42a8fab23b6bbd129dafae5693aa23c493b3eba
enzoleal-Abla <enzo.leal@abla.one>
2025-08-15 01:05:14 -0300

    Mais uma parte da aula 14

--- aula14/index.js
+++ aula14/index.js
@@ -1,5 +1,9 @@
 let num1 = 1;
 let num2 = 2.5;
+let num3 = 2.54235346;
 
 console.log(num1.toString() + num2.toString());
 console.log(typeof num1);
+console.log(num3.toFixed(2)); // Para melhorar visualização de float
+console.log(num3.toFixed(0));
+console.log(Number.isInteger(num3), Number.isInteger(num1)); // Saber tipo de numero
