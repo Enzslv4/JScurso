ac3cdf4b47338d819c0682b340ae83fe66a05ebe
enzoleal-Abla <enzo.leal@abla.one>
2026-02-01 19:41:02 -0300

    aulas

---
+++ aula_25/index.js
@@ -0,0 +1,11 @@
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log("Não está entre 0 e 5")
+} else if (numero >=6 && numero <= 8){
+    console.log("menor q 8")
+} else if (numero >=9 && numero <= 11){
+    console.log("maoir q 8")
+} else {
+    console.log("Não")
+}
