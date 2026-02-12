9e1196dc870b2f9b6d31274af7ed7f9b2f529fb0
enzoleal-Abla <enzo.leal@abla.one>
2026-02-12 16:42:43 -0300

    fim aula 26

--- aula_26/assets/js/main.js
+++ aula_26/assets/js/main.js
@@ -10,8 +10,6 @@ form.addEventListener('submit', function (e) {
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
-    console.log(peso, altura);
-
     if (!peso && !altura) {
         setResultado("Verifique os dados enviados.", false);
         return;
@@ -29,14 +27,27 @@ form.addEventListener('submit', function (e) {
 
     const imc = getIMC(peso, altura);
     setResultado(`Seu IMC é: ${imc}`, false);
+
+    const nivelImc = getNivelIMC(imc);
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`
+
+    setResultado(msg, true);
 });
 
 function getNivelIMC (imc) {
-    const nivel = ["Abaixo do nível", "Peso Normal", "Sobrepeso", ""];
+    const nivel = ["Abaixo do nível", "Peso Normal", "Sobrepeso", "Obesidade grau 1", "Obesidade grau 2", "Obesidade grau 3"];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 };
 
 function getIMC(peso, altura) {
     const imc = peso / altura ** 2;
+
     return imc.toFixed(2);
 };
 
@@ -49,7 +60,16 @@ function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
 
+    
+
     const p = criaP(msg);
+
+    if (isValid) {
+        p.classList.add("paragrafo-resultado")
+    } else {
+        p.classList.add("bad")
+    }
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 };
