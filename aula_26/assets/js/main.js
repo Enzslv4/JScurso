406c7046d56f958491b5094779779d520825ac7e
enzoleal-Abla <enzo.leal@abla.one>
2026-02-12 16:04:14 -0300

    tt aula 26

--- aula_26/assets/js/main.js
+++ aula_26/assets/js/main.js
@@ -3,17 +3,53 @@ const form = document.querySelector("#formulario");
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log("Evento Previnido.");
-    setResultado("teste");
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    console.log(peso, altura);
+
+    if (!peso && !altura) {
+        setResultado("Verifique os dados enviados.", false);
+        return;
+    };
+
+    if (!peso) {
+        setResultado("Peso inválido!", false);
+        return;
+    };
+
+    if (!altura) {
+        setResultado("Altura inválida!", false);
+        return;
+    };
+
+    const imc = getIMC(peso, altura);
+    setResultado(`Seu IMC é: ${imc}`, false);
 });
 
-function setResultado (msg) {
+function getNivelIMC (imc) {
+    const nivel = ["Abaixo do nível", "Peso Normal", "Sobrepeso", ""];
+};
+
+function getIMC(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+};
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+};
+
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
 
-    const p = document.createElement('p');
-    p.classList.add('paragrafo-resultado')
+    const p = criaP(msg);
     p.innerHTML = msg;
-
     resultado.appendChild(p);
 };
