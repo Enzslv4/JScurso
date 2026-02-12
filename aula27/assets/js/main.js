e9fb1bb9770ca2c9b5d0c64e4e55ece9b4633c12
enzoleal-Abla <enzo.leal@abla.one>
2026-02-12 16:59:49 -0300

    inicio aula 28

---
+++ aula27/assets/js/main.js
@@ -0,0 +1,12 @@
+// condição ? valor verdadeiro : valor falso
+
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? "Usuário VIP" : "Usuário normal";
+
+console.log(nivelUsuario)
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log("VIP")
+// } else {
+//     console.log("Normal")
+// }
