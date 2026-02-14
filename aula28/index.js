4c2e57de5e2eed0bad0dec4997f8704c62ac5b8a
enzoleal-Abla <enzo.leal@abla.one>
2026-02-13 22:30:52 -0300

    inicio aula 29

--- aula28/index.js
+++ aula28/index.js
@@ -1,3 +1,18 @@
 const data = new Date();
 
-console.log(data);
+// const tresHoras = 60 * 60 * 3 * 1000; conta em milissegundos
+// const umDia = 60 * 60 * 24 * 1000;
+// const data = new Date(0 + tresHoras + umDia);
+// const data = new Date(2020, 3, 22, 8, 0, 0); a, m, d, h, m, s, ms
+// const data = new Date("2020-03-22 08:00:00");
+
+console.log("Dia", data.getDate());
+console.log("Mes", data.getMonth());
+console.log("Ano", data.getFullYear());
+console.log("Hora", data.getHours());
+console.log("Min", data.getMinutes());
+console.log("Seg", data.getSeconds());
+console.log("ms", data.getMilliseconds());
+console.log("Dia da Semana", data.getDay());
+
+console.log(data.toString());
