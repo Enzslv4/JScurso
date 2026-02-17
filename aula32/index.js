f6c6f519948bcc6be117a7467de4b6760ada589c
enzoleal-Abla <enzo.leal@abla.one>
2026-02-16 21:22:03 -0300

    aula 33

--- aula32/index.js
+++ aula32/index.js
@@ -1,5 +1,12 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const [primeiroNumero, segundoNumero, ...resto] = numeros;
+const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros; // rest operator
+const[um, , dois, , tres] = numeros; // esse é massa
 
-console.log(primeiroNumero, segundoNumero);
+console.log(um, dois, tres);
+console.log(primeiroNumero, segundoNumero, terceiroNumero);
 console.log(resto);
+
+const outrosnums = [[1,2,3],[4,5,6],[7,8,9]];
+const [,,[sete]] = outrosnums; // incrivel
+
+console.log(sete);
