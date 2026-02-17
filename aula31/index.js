72698a7726adc0401d92e37098a4e10edaf811b0
enzoleal-Abla <enzo.leal@abla.one>
2026-02-16 21:12:35 -0300

    inicio aula 32

---
+++ aula31/index.js
@@ -0,0 +1,11 @@
+// Let tem escopo de bloco {... bloco}
+// Var só tem escopo de função
+
+let nome = "enzo"; // criando
+var nome2 = "silva";
+
+if (true) {
+    let nome = "Tutorial"; // criando
+    console.log(nome, nome2);
+}
+
