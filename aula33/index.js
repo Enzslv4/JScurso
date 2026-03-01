b930c8f28500f934157efb00ffa9f96b2a0d4713
Enzslv4 <enzosilva1420002@gmail.com>
2026-03-01 09:45:17 -0300

    inicio aula 34

--- aula33/index.js
+++ aula33/index.js
@@ -1,1 +1,15 @@
-console.log("teste");
+const pessoa = {
+    nome: "Enzo",
+    sobrenome: "Leal",
+    idade: 34,
+    endereco: {
+        rua: "Rua dos bobos",
+        numero: 0
+    }
+}
+
+// Atribuição via desestruturação
+const { nome = '', sobrenome, idade } = pessoa; // o "nome = ''" é um valor para quando não tem valor atribuido para essa chave
+// nome: teste - para poder atribuir uma chave a uma variavel
+
+console.log(pessoa);
