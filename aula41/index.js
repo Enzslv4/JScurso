d90c6de90f49c0c90f9a8ccd3b21328110fc19bb
Enzslv4 <enzosilva1420002@gmail.com>
2026-03-13 21:22:22 -0300

    41 feita

---
+++ aula41/index.js
@@ -0,0 +1,7 @@
+function maiorNum (num1, num2) {
+    return x > y ? x : y
+}
+
+const numeros = [2, -2]
+
+console.log(`Maior número entre ${numeros[0]} e ${numeros[1]}: ${maiorNum(numeros[0], numeros[1])}`);
