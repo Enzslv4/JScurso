ccc4047931942e2cbdc562237719ebf84ced9c01
Enzslv4 <enzosilva1420002@gmail.com>
2026-03-16 23:00:49 -0300

    Aula 44 feita

--- aula44/index.js
+++ aula44/index.js
@@ -1,2 +1,23 @@
 // Try, catch e throw
 
+// try {
+//     console.log(jogo);
+// } catch (e) {
+//     console.log("Deu erro parceiro!");
+//     console.log(e);
+// }
+
+function soma(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+        throw("São precisos 2 números.");
+    }
+
+    return x + y;
+}
+
+try {
+    console.log(soma(1,2));
+    console.log(soma("1", 2))
+} catch (erro) {
+    console.log(erro);
+}
