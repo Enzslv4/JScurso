05caf666d8a19f06d303f87909d96e6ba0502558
Enzslv4 <enzosilva1420002@gmail.com>
2026-03-30 23:28:48 -0300

    Inicio aula 56

---
+++ aula55/index.js
@@ -0,0 +1,11 @@
+// Funções imediatas = IIFE: Immediately Invoked Function Expression
+
+// Basicamente a estrutura é: (function() {})()
+// Isso cria uma função anonima
+(function() {
+    const nome = "Enzo";
+    console.log(nome);
+})();
+const nome = "Leal";
+
+console.log(nome);
