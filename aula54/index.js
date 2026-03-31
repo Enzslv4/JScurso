40e7054922e9285aeb03fa710ce6c5aa797ee9ec
Enzslv4 <enzosilva1420002@gmail.com>
2026-03-30 23:21:18 -0300

    aula 54

--- aula54/index.js
+++ aula54/index.js
@@ -1,22 +1,39 @@
-function f1() {
+function f1(callback) {
     setTimeout(() => {
         console.log("f1");
-    }, 1000);
-}
-
-function f2() {
-    setTimeout(() => {
-        console.log("f2");
+        if (callback) callback();
     }, 2000);
 }
 
-function f3() {
+function f2(callback) {
     setTimeout(() => {
-        console.log("f3");
-    }, 3000);
+        console.log("f2");
+        if (callback) callback();
+    }, 1000);
 }
 
-f1();
-f2();
-f3();
-console.log("Hello world!");
+function f3(callback) {
+    setTimeout(() => {
+        console.log("f3");
+        if (callback) callback();
+    }, 0);
+}
+
+// f1();
+// f2();
+// f3();
+// console.log("Hello world!");
+
+f1(f1Callback);
+
+function f1Callback() {
+    f2(f2Callback);
+}
+
+function f2Callback() {
+    f3(f3Callback);
+}
+
+function f3Callback() {
+    console.log("Olá Mundo!");
+}
